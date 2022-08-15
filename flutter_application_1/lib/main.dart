d98f6c4708749f3a9603e8a3f3fde4acbf511b0b
Carmine Laudato <56676908+carminelau@users.noreply.github.com>
2022-08-15 21:44:44 +0200

    Update Scroll Sign In

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -109,131 +109,143 @@ class SignIn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assert/strada_al_tramonto.jpg"),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Column(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 60, left: 0, right: 300, bottom: 15),
-            child: ElevatedButton.icon(
-                label: Text(''),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.deepPurpleAccent,
-                  onPrimary: Colors.white,
-                  textStyle: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.w600),
-                  fixedSize: const Size(40, 40),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50)),
-                ),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return HomePage(title: 'SignIn');
-                  }));
-                },
-                icon: Icon(Icons.arrow_circle_left_rounded)),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 10),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Nome',
-                  fillColor: Colors.white70,
-                  filled: true),
+            constraints: BoxConstraints.expand(),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assert/strada_al_tramonto.jpg"),
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 0, left: 15, right: 15, bottom: 10),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Cognome',
-                  fillColor: Colors.white70,
-                  filled: true),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 0, left: 15, right: 15, bottom: 10),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Data di Nascita',
-                  fillColor: Colors.white70,
-                  filled: true),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 0, left: 15, right: 15, bottom: 10),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Città (PROV)',
-                  fillColor: Colors.white70,
-                  filled: true),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 0, left: 15, right: 15, bottom: 10),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Indirizzo',
-                  fillColor: Colors.white70,
-                  filled: true),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 0, left: 15, right: 15, bottom: 10),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Username',
-                  fillColor: Colors.white70,
-                  filled: true),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 0, left: 15, right: 15, bottom: 10),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Password',
-                  fillColor: Colors.white70,
-                  filled: true),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 0, left: 15, right: 15, bottom: 10),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Confirmed Password',
-                  fillColor: Colors.white70,
-                  filled: true),
-            ),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: Colors.blue,
-              onPrimary: Colors.white,
-              textStyle:
-                  const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-              fixedSize: const Size(120, 40),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50)),
-            ),
-            child: Text('Sign In'),
-            onPressed: () {},
-          )
-        ],
-      ),
-    ));
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: 60, left: 0, right: 300, bottom: 15),
+                    child: ElevatedButton.icon(
+                        label: Text(''),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.deepPurpleAccent,
+                          onPrimary: Colors.white,
+                          textStyle: const TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w600),
+                          fixedSize: const Size(40, 40),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                        ),
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return HomePage(title: 'SignIn');
+                          }));
+                        },
+                        icon: Icon(Icons.arrow_circle_left_rounded)),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: 10, left: 15, right: 15, bottom: 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Nome',
+                          fillColor: Colors.white70,
+                          filled: true),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: 0, left: 15, right: 15, bottom: 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Cognome',
+                          fillColor: Colors.white70,
+                          filled: true),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: 0, left: 15, right: 15, bottom: 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Data di Nascita',
+                          fillColor: Colors.white70,
+                          filled: true),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: 0, left: 15, right: 15, bottom: 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Città (PROV)',
+                          fillColor: Colors.white70,
+                          filled: true),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: 0, left: 15, right: 15, bottom: 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Indirizzo',
+                          fillColor: Colors.white70,
+                          filled: true),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: 0, left: 15, right: 15, bottom: 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Username',
+                          fillColor: Colors.white70,
+                          filled: true),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: 0, left: 15, right: 15, bottom: 10),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Password',
+                          fillColor: Colors.white70,
+                          filled: true),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: 0, left: 15, right: 15, bottom: 10),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Confirmed Password',
+                          fillColor: Colors.white70,
+                          filled: true),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      onPrimary: Colors.white,
+                      textStyle: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.w600),
+                      fixedSize: const Size(120, 40),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)),
+                    ),
+                    child: Text('Sign In'),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            )));
   }
 }
